3b12fd2f1b9b4592113955fc99dedd1ac3d09f97
Leonardo Mufato <leonardo.mufato@studenti.unipd.it>
2022-05-23 16:07:30 +0200

    plus Images

--- the_best_app/lib/main.dart
+++ the_best_app/lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:prova_project/screens/experiencePage.dart';
-import 'package:prova_project/screens/homePage.dart';
-import 'package:prova_project/screens/preferencesPage.dart';
-import 'package:prova_project/screens/shoppingPage.dart';
+//LoginScreens
+import 'package:prova_project/Screens/LoginScreens/LoginPage.dart';
+import 'package:prova_project/Screens/LoginScreens/ForgotPasswordPage.dart';
+import 'package:prova_project/Screens/LoginScreens/HelloWordPage.dart';
+import 'package:prova_project/Screens/LoginScreens/RegistrationPage.dart';
+// HomeScreens
+import 'package:prova_project/Screens/HomeScreens/HomePage.dart';
+
+import 'package:prova_project/Screens/experiencePage.dart';
+import 'package:prova_project/Screens/preferencesPage.dart';
+import 'package:prova_project/Screens/shoppingPage.dart';
 //TODO: import the homepage widget
 
 void main() {
@@ -14,17 +21,49 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-          scaffoldBackgroundColor: Colors.green[100],
-          primarySwatch: Colors.green),
-      initialRoute: HomePage.route,
-      //This maps names to the set of routes within the app
-      routes: {
-        HomePage.route: (context) => HomePage(),
-        PrefPage.route: (context) => PrefPage(),
-        Opt1Page.route: (context) => Opt1Page(),
-        Opt2Page.route: (context) => Opt2Page(),
-      },
-    );
+        theme: ThemeData(
+            scaffoldBackgroundColor: Colors.green[100],
+            primarySwatch: Colors.green),
+        initialRoute: HelloWordPage.route,
+        onGenerateRoute: (settings) {
+          if (settings.name == HelloWordPage.route) {
+            return MaterialPageRoute(builder: (context) {
+              return HelloWordPage();
+            });
+          } else if (settings.name == LoginPage.route) {
+            return MaterialPageRoute(builder: (context) {
+              return LoginPage();
+            });
+          } else if (settings.name == HomePage.route) {
+            final args1 = settings.arguments as Map;
+            return MaterialPageRoute(builder: (context) {
+              return HomePage(
+                username: args1['username'],
+              );
+            });
+          } else if (settings.name == RegistrationPage.route) {
+            return MaterialPageRoute(builder: (context) {
+              return RegistrationPage();
+            });
+          } else if (settings.name == ForgotPasswordPage.route) {
+            return MaterialPageRoute(builder: (context) {
+              return ForgotPasswordPage();
+            });
+          } else if (settings.name == Opt2Page.route) {
+            return MaterialPageRoute(builder: (context) {
+              return Opt2Page();
+            });
+          } else if (settings.name == Opt1Page.route) {
+            return MaterialPageRoute(builder: (context) {
+              return Opt1Page();
+            });
+          } else if (settings.name == PrefPage.route) {
+            return MaterialPageRoute(builder: (context) {
+              return PrefPage();
+            });
+          } else {
+            return null;
+          }
+        });
   } //build
 }//MyApp
