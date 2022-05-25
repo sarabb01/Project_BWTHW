c78f02710ffae44c3c3527b6156ea101d948e187
Leonardo Mufato <leonardo.mufato@studenti.unipd.it>
2022-05-25 12:02:33 +0200

    Database Created (To implement app functionalities)

--- the_best_app/lib/Database/Daos/User_Cred_daos.dart
+++ the_best_app/lib/Database/Daos/User_Cred_daos.dart
@@ -1,24 +1,24 @@
 import 'dart:ffi';
 
 import 'package:floor/floor.dart';
+import 'package:prova_project/Classes/Users_Credential.dart';
 import 'package:prova_project/Database/Entities/User_Creds.dart';
 
 @dao
 abstract class User_Crededentials_Dao {
-  @Query('SELECT username FROM User_Credentials')
-  Future<List<String>> findAllUsers();
+  @Query('SELECT username FROM Users_Credentials')
+  Future<List<String>?> findAllUsers();
 
-  @Query(
-      'SELECT username FROM User_Credentials WHERE id =: id AND username =: username')
-  Stream<String> findUser(int id, String username);
+  @Query('SELECT username FROM Users_Credentialss WHERE username = :username')
+  Future<String?> findUser(String username);
 
   @Insert(
       onConflict: OnConflictStrategy
           .rollback) // In ordert to avoid duplicates within the users
-  Future<List<String>> addUser(Users_Credentials user);
+  Future<void> addUser(Users_Credentials user);
 
   @Update(onConflict: OnConflictStrategy.replace)
-  Future<void> updateUserPassword(String password);
+  Future<void> updateUserPassword(Users_Credentials user);
 
   @delete
   Future<void> deleteUser(Users_Credentials user);
