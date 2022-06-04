e8d367ff5d7b4ad529c63c5b3f81847db701c9d7
Sara Barbacane <sarabarbacane.sb@gmail.com>
2022-06-04 11:37:30 +0200

    [Sara] details

--- the_best_app/lib/models/shopList.dart
+++ the_best_app/lib/models/shopList.dart
@@ -5,9 +5,9 @@ class shopList {
     'Non Solo Sport': [_maxPoints * 0.5, 'Non Solo Sport', '20% discount'],
     'Sportler': [_maxPoints * 0.6, 'Sportler', '15% discount'],
     'Cisalfa Sport': [_maxPoints * 0.7, 'Cisalfa Sport', '15% discount'],
-    'AW Lab': [_maxPoints * 0.75, '10% discount'],
-    'Foot Locker': [_maxPoints * 0.85, '10% discount'],
-    'Nike': [_maxPoints * 0.9, '10% discount'],
-    'Adidas': [_maxPoints * 0.9, '10% discount'],
+    'AW Lab': [_maxPoints * 0.75, 'AW Lab', '10% discount'],
+    'Foot Locker': [_maxPoints * 0.85, 'Foot Locker', '10% discount'],
+    'Nike': [_maxPoints * 0.9, 'Nike', '10% discount'],
+    'Adidas': [_maxPoints * 0.9, 'Adidas', '10% discount'],
   };
 }
