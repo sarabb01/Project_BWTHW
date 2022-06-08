3d6776ec06cd3a2276aded5f37157a70acc5b3ee
Matteo09 <sproc98@gmail.com>
2022-06-08 11:42:06 +0200

    new pages

--- the_best_app/lib/screens/profiledatapage.dart
+++ the_best_app/lib/screens/profiledatapage.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 class Profiledatapage extends StatelessWidget {
   Profiledatapage({Key? key}) : super(key: key);
 
-  static const route = '/Profiledatapage';
+  static const route =
+      '/hellowordpage/loginpage/homepage/profilepage/profiledatapage';
   static const routename = 'Profile data';
 
   @override
@@ -14,22 +15,69 @@ class Profiledatapage extends StatelessWidget {
           title: Text(Profiledatapage.routename),
         ),
         body: Drawer(
-            child: ListView(
-          children: <Widget>[
-            ListTile(
-              title: Text('Modifica Profilo'),
-              trailing: Icon(Icons.more_vert),
-            ),
-            ListTile(
-              title: Text('Cancella i miei dati'),
-              trailing: Icon(Icons.delete),
-            ),
-            ListTile(
-              title: Text('Logout'),
-              trailing: Icon(Icons.logout),
-            ),
-          ],
-        )));
+            child: SizedBox(
+                height: 200,
+                child: ListView(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(
+                          left: 10, right: 10, top: 5, bottom: 5),
+                      child: ListTile(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          //side: BorderSide(color: Colours.azure)
+                        ),
+                        title: const Text('Modify my profile'),
+                        trailing: IconButton(
+                          icon: Icon(Icons.more_vert),
+                          //heroTag: 'btn1',
+                          onPressed: () {
+                            Navigator.pop(
+                                context, '/hellowordpage/registrationpage');
+                          },
+                        ),
+                        tileColor: Colors.green[100],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                          left: 10, right: 10, top: 5, bottom: 5),
+                      child: ListTile(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          //side: BorderSide(color: Colours.azure)
+                        ),
+                        title: const Text('Logout'),
+                        trailing: IconButton(
+                          icon: Icon(Icons.logout),
+                          //heroTag: 'btn2',
+                          onPressed: () {},
+                        ),
+                        tileColor: Colors.green[100],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                          left: 10, right: 10, top: 5, bottom: 10),
+                      child: ListTile(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          //side: BorderSide(color: Colours.azure)
+                        ),
+                        title: const Text('Remove my account'),
+                        trailing: IconButton(
+                          icon: Icon(Icons.delete),
+
+                          //heroTag: 'btn3',
+                          onPressed: () {
+                            Navigator.pop(context, '/hellowordpage');
+                          },
+                        ),
+                        tileColor: Colors.green[100],
+                      ),
+                    )
+                  ],
+                ))));
   } //build
 
 } //Page
