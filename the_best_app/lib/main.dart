767ab7e9f7d85a104c8e7100af2f7209045fa62b
Sara Barbacane <sarabarbacane.sb@gmail.com>
2022-06-19 20:53:48 +0200

    [sara] add cnp

--- the_best_app/lib/main.dart
+++ the_best_app/lib/main.dart
@@ -7,6 +7,7 @@ import 'package:the_best_app/Screens/LoginScreens/LoginPage.dart';
 import 'package:the_best_app/Screens/LoginScreens/ForgotPasswordPage.dart';
 import 'package:the_best_app/Screens/LoginScreens/HelloWordPage.dart';
 import 'package:the_best_app/Screens/LoginScreens/RegistrationPage.dart';
+import 'package:the_best_app/models/pointsModel.dart';
 import 'package:the_best_app/screens/PointsScreens/summaryPage.dart';
 import 'package:the_best_app/screens/infopage.dart';
 // Home Screens
@@ -42,8 +43,17 @@ Future<void> main() async {
   final users_database_repo = UsersDatabaseRepo(database: database);
   //Here, we run the app and we provide to the whole widget tree the instance of the DatabaseRepository.
   //That instance will be then shared through the platform and will be unique.
-  runApp(ChangeNotifierProvider<UsersDatabaseRepo>(
-    create: (context) => users_database_repo,
+  // runApp(ChangeNotifierProvider<UsersDatabaseRepo>(
+  //   create: (context) => users_database_repo,
+  //   child: MyApp(),
+  // ));
+
+  runApp(MultiProvider(
+    providers: [
+      ListenableProvider<UsersDatabaseRepo>(
+          create: (context) => users_database_repo),
+      ListenableProvider<PointsModel>(create: (context) => PointsModel())
+    ],
     child: MyApp(),
   ));
 } //main
