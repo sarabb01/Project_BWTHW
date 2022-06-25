7f89dd798ad7954e9c34c649db1ae055ca567062
Sara Barbacane <sarabarbacane.sb@gmail.com>
2022-06-25 17:05:33 +0200

    [Sara] 'none' modified

--- the_best_app/lib/models/targetTypes.dart
+++ the_best_app/lib/models/targetTypes.dart
@@ -1,7 +1,7 @@
 class Target {
   Map<String, List> targets = {
     // STEPS, CLAS, CARDIO, SLEEP
-    'None': [10000, 600, 10, 7],
+    'None': [8000, 400, 10, 7],
     'Basic': [10000, 600, 10, 7],
     'Medium': [12000, 800, 15, 7],
     'Advanced': [15000, 1000, 20, 7]
