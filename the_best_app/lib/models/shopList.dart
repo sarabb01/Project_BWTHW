2d4974dcc25433ec186a3941358a170d8e1b67bd
Sara Barbacane <sarabarbacane.sb@gmail.com>
2022-06-26 14:18:45 +0200

    [Sara] added photos QR and path

--- the_best_app/lib/models/shopList.dart
+++ the_best_app/lib/models/shopList.dart
@@ -3,13 +3,48 @@ import 'dart:ffi';
 class shopList {
   static double _minPoints = 120.0;
   Map<String, List> Catalog = {
-    'Decathlon': [_minPoints * 1, 'Decathlon', '20% discount'],
-    'Non Solo Sport': [_minPoints * 1.5, 'Non Solo Sport', '20% discount'],
-    'Sportler': [_minPoints * 2, 'Sportler', '15% discount'],
-    'Cisalfa Sport': [_minPoints * 2.5, 'Cisalfa Sport', '15% discount'],
-    'AW Lab': [_minPoints * 3.5, 'AW Lab', '10% discount'],
-    'Foot Locker': [_minPoints * 3.5, 'Foot Locker', '10% discount'],
-    'Nike': [_minPoints * 5, 'Nike', '10% discount'],
-    'Adidas': [_minPoints * 5, 'Adidas', '10% discount'],
+    'Decathlon': [
+      _minPoints * 1,
+      'Decathlon',
+      '20% discount',
+      'assets/QRcodes/Decathlon.png'
+    ],
+    'Non Solo Sport': [
+      _minPoints * 1.5,
+      'Non Solo Sport',
+      '20% discount',
+      'assets/QRcodes/NonSoloSport.png'
+    ],
+    'Sportler': [
+      _minPoints * 2,
+      'Sportler',
+      '15% discount',
+      'assets/QRcodes/Sportler.png'
+    ],
+    'Cisalfa Sport': [
+      _minPoints * 2.5,
+      'Cisalfa Sport',
+      '15% discount',
+      'assets/QRcodes/Cisalfa.png'
+    ],
+    'AW Lab': [
+      _minPoints * 3.5,
+      'AW Lab',
+      '10% discount',
+      'assets/QRcodes/AWLab.png'
+    ],
+    'Foot Locker': [
+      _minPoints * 3.5,
+      'Foot Locker',
+      '10% discount',
+      'assets/QRcodes/Footlocker.png'
+    ],
+    'Nike': [_minPoints * 5, 'Nike', '10% discount', 'assets/QRcodes/Nike.png'],
+    'Adidas': [
+      _minPoints * 5,
+      'Adidas',
+      '10% discount',
+      'assets/QRcodes/Adidas.png'
+    ],
   };
 }
